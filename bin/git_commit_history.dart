33fa8ea19f789d975a1380d529734b09d6aa4590
luissantana1 <luissantanareynoso@gmail.com>
2023-11-01 13:55:19 -0400

    Just the first commit, with the basic file structure for a dart console project

---
+++ bin/git_commit_history.dart
@@ -0,0 +1,5 @@
+import 'package:git_commit_history/git_commit_history.dart' as git_commit_history;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_commit_history.calculate()}!');
+}
---
+++ test/git_commit_history_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_commit_history/git_commit_history.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
